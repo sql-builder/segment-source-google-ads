8b1d08fc1c009690a6eaed7d492556731be1c949
Matt Strautmann <matt.strautmann@goodmoney.com>
2021-03-09 15:48:16 -0800

    clean up definition

--- definitions/segment_google_ads_source.js
+++ definitions/segment_google_ads_source.js
@@ -2,30 +2,16 @@ const google = require("../");
 
 google({
     //
-    // Define the database and schema of your Google data
-    // databaseName: "database_name",
-    // schemaName: "schema_name",
+    Define the database and schema of your Google data
+    databaseName: "database_name",
+    schemaName: "schema_name",
     // Optional prefix metadata if applicable
-    // stagingTablePrefix: "stg_",
-    // // Define the destination schema and table/view
-    // defaultConfig: {
-    //     database: "database_name",
-    //     schema: "schema_name",
-    //     tags: ["google"],
-    //     type: "table"
-
-        //
-        // Define the database and schema of your facebook data
-    databaseName: "raw",
-    schemaName: "google_ads",
-        // Optional prefix metadata if applicable
-        stagingTablePrefix: "stg_",
-        // Define the destination schema and table/view
-        defaultConfig: {
-            database: "dv_prototype_db",
-            schema: "dataform",
-            tags: ["google"],
-    type: "view"
-
+    stagingTablePrefix: "stg_",
+    // Define the destination schema and table/view
+    defaultConfig: {
+        database: "database_name",
+        schema: "schema_name",
+        tags: ["google"],
+        type: "table"
     },
 });
