ebb9dfcc5219e3a595f035f5eba01ddf6a3a0bed
Matt Strautmann <matt.strautmann@gmail.com>
2021-10-24 14:19:54 -0700

    Update segment_google_campaigns.js

--- includes/segment_google_campaigns.js
+++ includes/segment_google_campaigns.js
@@ -7,8 +7,6 @@ module.exports = (params) => {
     return publish(params.stagingTablePrefix + "segment_google_ad_campaigns", {
         ...params.defaultConfig
     }).query(ctx => `
-with source as (
-    ${crossDB.filterSegment(ctx, params, tableName, `id`)} )
 SELECT
     cast(id as string) as ad_campaign_id,
     cast(adwords_customer_id as string) as ad_account_id,
@@ -21,16 +19,6 @@ SELECT
     uuid_ts as row_loaded_on,
     current_timestamp() as updated_on,
     'Segment' as source_name
-FROM source
+FROM ${ctx.ref({ database: params.sourceDatabaseName, schema: params.sourceSchemaName, name: `${tableName}` })}
 `)
 }
-
-// Campaigns
-// PROPERTY NAME	DESCRIPTION
-// adwords_customer_id	The 10 - digit Google Ads Customer ID.
-// end_date	Date the campaign ends.
-// name	Name of this campaign.
-// received_at	This timestamp is added to incoming messages as soon as they hit Segment API.
-// serving_status	Serving status of the campaign.
-// start_date	Date the campaign begins.
-// status	Status of the campaign.
