38bd4fa424d8242c99397fee17c51c58faa0e4e4
Matt Strautmann <matt.strautmann@gmail.com>
2021-10-24 14:18:35 -0700

    Update segment_google_ad_groups.js

--- includes/segment_google_ad_groups.js
+++ includes/segment_google_ad_groups.js
@@ -7,9 +7,6 @@ module.exports = (params) => {
     return publish(params.stagingTablePrefix + "segment_google_ad_groups", {
         ...params.defaultConfig
     }).query(ctx => `
-with source as (
-    ${crossDB.filterSegment(ctx, params, tableName, `id`)}
-)
 SELECT
     cast(id as string) as ad_group_id,
     cast(campaign_id as string) as ad_campaign_id,
@@ -25,14 +22,6 @@ SELECT
     uuid_ts as row_loaded_on,
     current_timestamp() as updated_on,
     'Segment' as source_name
-FROM source
+FROM ${ctx.ref({ database: params.sourceDatabaseName, schema: params.sourceSchemaName, name: `${tableName}` })}
 `)
 }
-
-// Ad Groups
-// PROPERTY NAME	DESCRIPTION
-// adwords_customer_id	The 10 - digit Google Ads Customer ID.
-// campaign_id	ID of the campaign with which this ad group is associated.
-// name	Name of this ad group.
-// received_at	This timestamp is added to incoming messages as soon as they hit Segment API.
-// status	Status of this ad group.
