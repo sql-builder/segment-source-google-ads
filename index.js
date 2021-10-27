f895a10c4bb6d86c47cf2da24c584f507aec1e0a
Matt Strautmann <matt.strautmann@gmail.com>
2021-10-27 10:06:36 -0700

    Add campaign performance table.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const segmentGoogleAds = require("./includes/segment_google_ads");
 const segmentGoogleAdsets = require("./includes/segment_google_ad_groups");
 const segmentGoogleCampaigns = require("./includes/segment_google_campaigns");
 const segmentGoogleAdPerformance = require("./includes/segment_google_ad_performance");
+const segmentGoogleCampaignPerformance = require("./includes/segment_google_campaign_performance");
 
     
 module.exports = (params) => {
@@ -25,7 +26,7 @@ module.exports = (params) => {
         stagingTablePrefix,
     } = params;
 
-    let ads, adGroups, campaigns, adPerformance;
+    let ads, adGroups, campaigns, adPerformance, campaignPerformance;
 
     ads = declare({
         ...params.defaultConfig,
@@ -54,12 +55,19 @@ module.exports = (params) => {
         name: "ad_performance_reports"
     });
 
+    campaignPerformance = declare({
+        ...params.defaultConfig, database: params.sourceDatabaseName,
+        schema: params.sourceSchemaName,
+        name: "campaign_performance_reports"
+    });
+    
     // Publish and return datasets.
     let result = {
         ads: segmentGoogleAds(params),
         adGroups: segmentGoogleAdsets(params),
         campaigns: segmentGoogleCampaigns(params),
-        adPerformance: segmentGoogleAdPerformance(params)
+        adPerformance: segmentGoogleAdPerformance(params),
+        campaignPerformance: segmentGoogleCampaignPerformance(params)
     };
 
     return result;
